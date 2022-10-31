eac7e3991e5599b897e294f4c5520b3766378ac5
kayongo <kayonbrian@gmail.com>
2022-10-31 23:54:40 +0200

    added a value for the empty list

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     for item in my_list[::-1]:
-        print("{:d}".format(item))
+        if my_list is not None:
+            print("{:d}".format(item))
+        else:
+            return ''
