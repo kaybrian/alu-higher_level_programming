f4aaef15669a1bcfe7add47392b7e5e298b9d201
kayongo <kayonbrian@gmail.com>
2022-10-31 23:34:51 +0200

    working with indexes

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -2,8 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return None
     else:
-        print("Element at index {:d} is {:d}".format(
-            idx, my_list[idx]))
+        return my_list[idx]
