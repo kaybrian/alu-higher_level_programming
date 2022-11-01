cefc26c55417eccf38ba27f47c5065b38c0ddc04
kayongo <kayonbrian@gmail.com>
2022-11-01 16:37:51 +0200

    finding the max value

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 1:
+        return my_list[0]
+
+    op1 = my_list[0]
+    op2 = max_integer(my_list[1:])
+
+    if op1 > op2:
+        return op1
+    else:
+        return op2
