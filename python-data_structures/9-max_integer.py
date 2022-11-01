7b5af649caad785193147663aa8029ad34da981c
kayongo <kayonbrian@gmail.com>
2022-11-01 16:41:46 +0200

    list largest value

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 1:
-        return my_list[0]
-
-    op1 = my_list[0]
-    op2 = max_integer(my_list[1:])
-
-    if op1 > op2:
-        return op1
+    if not my_list:
+        return None
     else:
-        return op2
+        if len(my_list) == 1:
+            return my_list[0]
+        else:
+            op1 = my_list[0]
+            op2 = max_integer(my_list[1:])
+
+            if op1 > op2:
+                return op1
+            else:
+                return op2
