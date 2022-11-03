0a11696059f8e5e76dc9c2cc4daa756157b1f7c0
kayongo <kayonbrian@gmail.com>
2022-11-03 17:39:25 +0200

    same lin

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list:
-        return []
     new_list = my_list.copy()
+
+    if not my_list:
+        return new_list
     for i in range(len(my_list)):
         if new_list[i] == search:
             new_list[i] = replace
