e1bc5454f1de5eee901b262dce10521aae2dd994
kayongo <kayonbrian@gmail.com>
2022-11-10 23:15:18 +0200

    create a private instance

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''Create a new class of Rectangle'''
+
+
+class Rectangle:
+    '''A class that creates a Rectangle class'''
+
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        '''Gives back the value of the width'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    def height(self):
+        '''Gives back the value of the width'''
+        return self.__height
+
+    @width.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__height = value
