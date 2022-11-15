9682926e61d5310765c779a6e6fcb26a7c4c44d7
kayongo <kayonbrian@gmail.com>
2022-11-15 10:19:35 +0200

    running the tsts

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,7 @@ class MyList(list):
     '''This Class inherits the built in function list'''
 
     def print_sorted(self):
-        sorted_list = sorted(self[:])
+        '''Prints the list in a sorted order  '''
+        sorted_list = self[:]
+        sorted_list.sort()
         print("{}".format(sorted_list))
