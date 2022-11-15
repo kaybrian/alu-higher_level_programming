b8fcaaf523005788d23392c3915e84c284ff98c2
kayongo <kayonbrian@gmail.com>
2022-11-15 10:32:49 +0200

    remove the white spaces

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,12 +6,13 @@ def is_kind_of_class(obj, a_class):
     '''Check if the object is an instance of a class
 
     Args:
-        obj: object to check 
+        obj: object to check
         a_class: class to check from
 
     Returns:
-        True: if the object is an instance of, or if the object is an instance of a class
-        False: if the object is not an instance of, or if the object is an instance of a class 
-
+        True: if the object is an instance 
+            of, or if the object is an instance of a class
+        False: if the object is not an instance 
+            of, or if the object is an instance of a class
     '''
     return isinstance(obj, a_class)
