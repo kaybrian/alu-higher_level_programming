176469139cade7f7f0c34f305ed1911577622285
kayongo <kayonbrian@gmail.com>
2022-11-21 12:13:57 +0200

    making and sending urls to a url

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''send emails on a url'''
+
+
+import urllib.request
+import sys
+
+
+if __name__ == '__main__':
+    """"Documented"""
+    url = sys.argv[1]
+    message = {
+        "email": sys.argv[2]
+    }
+    data = urllib.parse.urlencode(message)
+    data = data.encode('ascii')
+    with urllib.request.urlopen(url) as response:
+        cont = response.read()
+        print(f"{cont.decode('utf-8')}")
