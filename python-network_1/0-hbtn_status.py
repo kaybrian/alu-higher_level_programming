ca8e8cf561ddd4ab42b29654470610da167b9437
kayongo <kayonbrian@gmail.com>
2022-11-21 11:52:26 +0200

    documenting the modul

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-'''a Python script that fetches https://alu-intranet.hbtn.io/status
-'''
+'''a Python script that fetches a url'''
+
 
 import urllib.request
+
 
 res = urllib.request.Request("https://alu-intranet.hbtn.io/status")
 with urllib.request.urlopen(res) as resp:
     data = resp.read()
     print("Body response:")
-    print(f"\t- type:{type(data)}")
-    print(f"\t- content:{data}")
-    print(f"\t- utf8 content:{data.decode('utf-8')}")
+    print(f"\t- type: {type(data)}")
+    print(f"\t- content: {data}")
+    print(f"\t- utf8 content: {data.decode('utf-8')}")
