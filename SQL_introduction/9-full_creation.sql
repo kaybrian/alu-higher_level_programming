da48f2b1a1a5b4b9a6406054018d98d5185abbc6
Kayongo Johnson Brian <46319683+kaybrian@users.noreply.github.com>
2022-11-22 11:58:53 +0000

    full installation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- creates a table and then inserts things in it 
+CREATE TABLE IF NOT EXISTS second_table(
+    id int,
+    name VARCHAR(256),
+    score int
+);
+
+INSERT INTO second_table(id,name,score) VALUES(
+    (1,"John", 10),
+    (2,"Alex",3),
+    (3,"Bob",14)
+    (4,"George",8)
+);
