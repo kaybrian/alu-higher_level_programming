88d1482ef8c25257133535147e4fbc41826c7c02
kaybrian <kayonbrian@gmail.com>
2022-11-24 11:06:52 +0200

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 --- creates a user for the database 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+
 FLUSH PRIVILEGES;
