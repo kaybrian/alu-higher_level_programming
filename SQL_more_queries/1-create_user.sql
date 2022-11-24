853a089ee288975507bb6877e319e4213d5d3ba0
kaybrian <kayonbrian@gmail.com>
2022-11-24 13:55:24 +0200

    added the new changes

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- create use if not there 
 CREATE USER IF NOT EXISTS 'new_user_0d_1user'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL ON *.* TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
