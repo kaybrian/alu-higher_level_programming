344fe200255a36cd6cf65c472d03f77f6e0a0b4e
kaybrian <kayonbrian@gmail.com>
2022-11-24 15:46:47 +0200

    more sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- create use if not there 
-CREATE USER IF NOT EXISTS 'new_user_0d_1user'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO user_0d_1@localhost;
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS new_user_0d_1user@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO user_0d_1@localhost
