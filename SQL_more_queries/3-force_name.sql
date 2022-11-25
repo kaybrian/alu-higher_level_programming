cc47f98d09bd479b95a3ea3748f583320d13098d
kaybrian <kayonbrian@gmail.com>
2022-11-25 15:00:46 +0200

    creating a new table

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,3 @@
---force name 
-CREATE TABLE IF NOT EXISTS force_name(
-    id INT,
-    name VARCHAR(256) NOT NULL
-);
+-- a script that creates the table force_name
+-- on your MySQL server
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
