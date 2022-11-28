9899632418ea574741911530b097e7750a5a7b4e
kaybrian <kayonbrian@gmail.com>
2022-11-28 15:02:03 +0200

    tests/4-print_square.txt

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -5,9 +5,9 @@
 def print_square(size):
     '''FUnction to print the squares
     Args:
-        size : must be an integer 
-    Returns: 
-        prints the square 
+        size : must be an integer
+    Returns:
+        prints the square
     '''
     square_char = "#"
     if type(size) != int:
@@ -16,7 +16,7 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     if type(size) == float and size < 0:
         raise TypeError("size must be an integer")
-    
+
     for i in range(size):
         for j in range(size):
             print(square_char,end="")
