2874b6150b9622a1fc2cc8d7ddc2a83115858094
kaybrian <kayonbrian@gmail.com>
2022-11-28 13:32:10 +0200

    added a function to print the items of a matirx

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A function to sperate words in python"""
+
+
+def text_indentation(text):
+    if type(text) != str:
+        raise TypeError("text must be a string")
+    
+    for delim in ".:?":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)]
+        )
+
+    print("{}".format(text), end="")
