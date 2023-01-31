324cb71b8029e76cfaaf4170bdf082d367746c68
kaybrian <kayonbrian@gmail.com>
2023-01-31 20:05:09 +0200

    little tweak

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,19 +2,14 @@
 
 const args = process.argv.slice(2);
 
-const number = parseInt(args[0]);
-let square;
+const num = parseInt(args[0]);
 
-if (number === undefined) {
-  console.log('Missing size');
-} else {
-  for (let i = 0; i < number; i++) {
-    for (let j = 0; j < number; j++) {
-      square += 'x';
-    }
-    if (i !== number) {
-      square += '\n';
-    }
+if (num) {
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
   }
-  console.log(square);
+} else if (num < 0) {
+  console.log();
+} else {
+  console.log('Missing size');
 }
