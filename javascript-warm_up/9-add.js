fc14c0a7dfdbf114622a86530703e5276c50a73b
kaybrian <kayonbrian@gmail.com>
2023-01-31 20:10:42 +0200

    add two numbers

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -5,7 +5,7 @@ const args = process.argv.slice(2);
 const num1 = parseInt(args[0]);
 const num2 = parseInt(args[1]);
 
-function add(a, b) {
+function add (a, b) {
   let sum = a + b;
   console.log(sum);
 }
