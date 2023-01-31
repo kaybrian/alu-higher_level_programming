194989e06871c1d478b08bd8abaf0faeadcc5246
kaybrian <kayonbrian@gmail.com>
2023-01-31 19:55:57 +0200

    print a square in js

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,2 +1,23 @@
 #!/usr/bin/node
 
+const args = process.argv.slice(2);
+
+const number = parseInt(args[0]);
+let square ='';
+if (number === undefined) {
+  console.log('Missing size');
+} else {
+  if (isNaN(number)) {
+    console.log('Missing size');
+  } else {
+    if (typeof (number) === 'number') {
+      for(let i=0; i<number; i++) {
+        for(let j=0; j<number; j++) {
+          square += 'x';
+        }
+        square += '\n';
+      }
+      console.log(square);
+    }
+  }
+}
