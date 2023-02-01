e204df896b9932bf21a03d9e9046de94d556105b
kaybrian <kayonbrian@gmail.com>
2023-02-01 10:54:43 +0200

    style fixing

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,26 +1,26 @@
 #!/usr/bin/node
 
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print() {
+  print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
 
-  rotate() {
+  rotate () {
     const temp = this.height;
     this.height = this.width;
     this.width = temp;
   }
 
-  double() {
+  double () {
     this.height = this.height * 2;
     this.width = this.width * 2;
   }
